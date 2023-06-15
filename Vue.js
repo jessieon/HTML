a15a8d81547461864c1b9c57b13670da29b3faff
jessieon <136649856+jessieon@users.noreply.github.com>
2023-06-15 13:54:26 -0300

    Create Vue.js

---
+++ Vue.js
@@ -0,0 +1,43 @@
+#DOCTYPE   html
+< html   = lang 'english'
+< head>
+  < meta charset= 'UTF'
+<meta name= 'viewport' content= widht- widht
+  <title> todo list </title>
+
+</hear>
+<body>
+    <div class = 'container'>
+      <p>
+      <input placeholder = 'name' 'type' = 'text'
+class = 'form- control/ >< textarea > name = 'authorn'
+</p>
+<button type = 'submit' class = 'btn' btn primary'
+</p>
+  < button type = ' submit' class = 'btn btn primary 
+ > comentar < / button >
+</d>
+< body >
+    < div class = 'container'>
+      <h1> comentários </h1>
+< div class = 'form  - todo  form- group' >
+  <p> 
+  < input placeholder = 'name' type = text name = ' author'
+class = 'form - control ' />
+  </p >
+<p>
+  <textarea placeholder = 'comentarios' name = 'message'
+</p >
+< button type = ' submit ' class = 'btn primary ' >
+  < div />
+  < div class = ' list group' >
+    <div class = list group - item '>
+<p > mensagem de texto < /p > 
+< div >
+  < href = '#' title = ' excluir ' > excluir< /a>
+< /div > 
+<hr>
+    < body > 
+    < html >
+    
+  
