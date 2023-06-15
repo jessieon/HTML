be347ab558e3282e499922298d78b063e8f305c0
jessieon <136649856+jessieon@users.noreply.github.com>
2023-06-15 19:29:11 -0300

    Create CSS.js

---
+++ CSS.js
@@ -0,0 +1,24 @@
+<template >
+<div >
+< h1> {{ title }} < h1/>
+</div >
+</ template >
+
+<script>
+import 'title' from / title.vue '
+export default {
+component: {
+'title':  title 
+}
+data () }
+return {
+title : { ' Yoshi ' , ' Mario' , ' Ryo' }
+}
+}
+}
+< / script >
+< style>
+ h1 {
+ color: purple ;
+ }
+ < / script >
